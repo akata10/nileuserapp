2ebfdd098b4a94656cd5b413f66b7d23a564d235
unknown <akatadaniel@gmail.com>
2024-12-04 15:01:45 +0100

    creating model feilds

--- userapps/models.py
+++ userapps/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+GENDER=(
+    ('M', 'male'),
+    ('f', 'female')
+)
 
 class Profile(models.Model):
+    user = models.OneToOnefield(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=GENDER)
+    profile_pix = models.ImageField(upload_to='profile', blank=True,null=True)
 
     def __str__(self):
-        pass
+        return self.fullname
+
 
   
