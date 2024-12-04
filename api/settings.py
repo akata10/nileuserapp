bfd371c4000dfeb20ce7eb6352bca472478f0d7c
unknown <akatadaniel@gmail.com>
2024-12-04 13:36:38 +0100

    migrating and creating super user

--- api/settings.py
+++ api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'userapps.apps.userappsconfig'
+    'userapps.apps.UserappsConfig'
 ]
 
 MIDDLEWARE = [
