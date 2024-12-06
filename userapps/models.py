e0311a2c7b8aa1d814d4f55d74ecc9ea792026e4
unknown <akatadaniel@gmail.com>
2024-12-06 11:09:50 +0100

    model migration

--- userapps/models.py
+++ userapps/models.py
@@ -8,7 +8,7 @@ GENDER=(
 )
 
 class Profile(models.Model):
-    user = models.OneToOnefield(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=255)
     phone = models.CharField(max_length=50)
     gender = models.CharField(max_length=10, choices=GENDER)
